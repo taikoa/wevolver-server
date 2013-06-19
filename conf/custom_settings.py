745511fa9315e7e3f9bcb1c456ae23428bbc55f8
Javier Aguirre <contacto@javaguirre.net>
2013-06-19 15:31:20 +0200

    Adding database to travis

--- conf/custom_settings.py
+++ conf/custom_settings.py
@@ -15,8 +15,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'wevolve_test',                      # Or path to database file if using sqlite3.
-        'USER': 'wevolve_test',                      # Not used with sqlite3.
-        'PASSWORD': 'wevolve_test',                  # Not used with sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
